8af8b760724df5bfcc46e9f540ba35c8d81f6e12
Gabriel Palladini <gmpalladini@gmail.com>
2020-09-05 16:41:26 +0200

    d5commit3

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -4,15 +4,17 @@ import "./style.css";
 const Navbar = () => {
     return (
         <nav className="navbar">
-        <div className="brand-title"> McMakler</div>
-        <a href="#" className="toggle-button">
-          <span className="bar"></span>
-          <span className="bar"></span>
-          <span className="bar"></span>
-        </a>
+            <div className="left-navbar">
+                <a href="#" className="toggle-button">
+                    <span className="bar"></span>
+                    <span className="bar"></span>
+                    <span className="bar"></span>
+                </a>
+                <div className="brand-title"> McMakler</div>
+            </div>
         <div className="navbar-links">
           <ul>
-            <li><a href="#"> Contact Support</a></li>
+            <li><img src="/icons/first.png" width='30' height='30' alt=''/><a href="#"> Contact Support </a></li>
             <li><a href="#"> Message icon</a></li>
             <li><a href="#"> User icon</a></li>
             <li><a href="#"> Turn Off icon </a></li>
